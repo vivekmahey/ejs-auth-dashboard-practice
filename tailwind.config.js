f6e536bc97816cbbfc0ef2300682f40a16494a61
vivekmahey <vivekmahey2244@gmail.com>
2025-08-07 19:07:38 +0530

    Improved and apllied tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: ["./views/**/*.ejs", "./public/**/*.html"],
+}
+
